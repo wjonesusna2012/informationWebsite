626c6a6c1db020d6b2e6a52ae0bb44d2d33cff93
wjonesusna2012 <44551434+wjonesusna2012@users.noreply.github.com>
2022-11-26 22:23:53 -0600

    Added in temporary image from public folder to display when loading src image.

--- my-app/src/TopicCard.tsx
+++ my-app/src/TopicCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
@@ -7,14 +7,29 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TopicCardProps } from './interfaces';
 
-
-const TopicCard = ({headerTitle, postTitle, summary, date}: TopicCardProps) => {
+const TopicCard = ({
+  headerTitle,
+  postTitle,
+  summary,
+  date
+}: TopicCardProps) => {
+  const [loaded, setLoaded] = useState(false);
   return (
-    <Card variant="outlined" sx={{maxWidth: 500}}>
+    <Card variant="outlined" sx={{ maxWidth: 500 }}>
       <CardMedia
         component="img"
         image="https://picsum.photos/200/100"
         alt="Test Alt Text"
+        onLoad={() => {
+          setLoaded(true);
+        }}
+        style={loaded ? {} : { display: 'none' }}
+      />
+      <CardMedia
+        component="img"
+        image="web.png"
+        alt="Test Alt Text"
+        style={loaded ? { display: 'none' } : { width: '100%', maxHeight: 250 }}
       />
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -26,15 +41,13 @@ const TopicCard = ({headerTitle, postTitle, summary, date}: TopicCardProps) => {
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           {summary}
         </Typography>
-        <Typography variant="body2">
-          {date}
-        </Typography>
+        <Typography variant="body2">{date}</Typography>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
   );
-}
+};
 
-export default TopicCard
+export default TopicCard;
